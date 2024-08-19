13ee2379504b3ccc72ce7e0975eab867b00a62ff
BDA824 <ferneyavila73@gmail.com>
2024-08-19 13:37:32 -0500

    Primer commit

--- App.js
+++ App.js
@@ -4,8 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.boxStart}>
+        <Text>A</Text>
+      </View>
+      <View style={styles.boxMid}>
+        <View style={styles.boxSec}></View>
+        <View style={styles.boxSec}></View>
+        <View style={styles.boxSec}></View>
+        <View style={styles.boxSec}></View>
+        <View style={styles.boxSec}></View>
+        <View style={styles.boxSec}></View>
+      </View>
+      <View style={styles.boxBottom}></View>
     </View>
   );
 }
@@ -14,7 +24,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
   },
+  boxStart: {
+    height: 165,
+    backgroundColor: '#868686',
+    alignContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+    margin: 10
+  },
+  boxMid: {
+    height: 285,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    alignContent: 'center',
+    gap: 30
+  },
+  boxSec: {
+    height: 75,
+    width: 75,
+    backgroundColor: '#868686',
+    borderRadius: 10
+  },
+  boxBottom: {
+    backgroundColor: '#868686',
+    height: 165,
+    borderRadius: 15,
+    margin: 10
+  }
 });
